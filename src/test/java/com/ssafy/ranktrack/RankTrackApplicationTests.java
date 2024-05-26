4f21c7a444b47dcaab99c1197d8e4a1ec198ea85
thswltjr11 <thswltjr11@gmail.com>
2024-05-26 16:31:48 +0900

    first commit

---
+++ src/test/java/com/ssafy/ranktrack/RankTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ranktrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
