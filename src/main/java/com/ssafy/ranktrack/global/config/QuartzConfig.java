5e2c6ab8fd85bda70779cbcc0af8db5ad59f66fe
thswltjr11 <thswltjr11@gmail.com>
2024-06-03 00:18:06 +0900

    refactor: Update 로직 분리

--- src/main/java/com/ssafy/ranktrack/global/config/QuartzConfig.java
+++ src/main/java/com/ssafy/ranktrack/global/config/QuartzConfig.java
@@ -6,29 +6,39 @@ import org.quartz.JobDetail;
 import org.quartz.SimpleScheduleBuilder;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class QuartzConfig {
+    @Value("${scheduler.enabled}")
+    private boolean isSchedulerEnabled;
 
     @Bean
     public JobDetail jobDetail() {
+        if (!isSchedulerEnabled) {
+            return null;
+        }
         return JobBuilder.newJob(UpdateJob.class)
-            .withIdentity("updateJob")
-            .storeDurably()
-            .build();
+                .withIdentity("updateJob")
+                .storeDurably()
+                .build();
     }
 
     @Bean
     public Trigger trigger(JobDetail jobDetail) {
+        if (!isSchedulerEnabled) {
+            return null;
+        }
         return TriggerBuilder.newTrigger()
-            .forJob(jobDetail)
-            .withIdentity("updateTrigger")
-            .withSchedule(SimpleScheduleBuilder.simpleSchedule()
-                .withIntervalInHours(24)
-                .repeatForever())
-            .build();
+                .forJob(jobDetail)
+                .withIdentity("updateTrigger")
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInHours(24)
+                        .repeatForever())
+                .build();
     }
 }
 
