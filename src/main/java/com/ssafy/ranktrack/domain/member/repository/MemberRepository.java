39839acc74d6c69ed4f0db4c46f2cf3c187fa511
thswltjr11 <thswltjr11@gmail.com>
2024-06-03 00:41:07 +0900

    feat: 네이티브 쿼리를 활용하여 rank 쿼리 도입

--- src/main/java/com/ssafy/ranktrack/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ssafy/ranktrack/domain/member/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.ranktrack.domain.member.repository;
 import com.ssafy.ranktrack.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -13,4 +14,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("SELECT m FROM Member m ORDER BY m.rating DESC")
     List<Member> findAllDesc() throws SQLException;
+    @Query(value = "SELECT r.rank FROM (SELECT handle, RANK() OVER (ORDER BY rating DESC) AS `rank` FROM member) r WHERE r.handle = :handle", nativeQuery = true)
+    Integer findRankByHandle(@Param("handle") String handle);
+    List<Member> findNearByHandle(String handle) throws SQLException;
+
 }
